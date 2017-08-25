60281d0ac3b11c64e6c90987898c2971c8c049ae
gyu <yugd@live.cn>
2017-08-25 09:52:32 +0200

    add constants

--- units_physics/constants_phys.py
+++ units_physics/constants_phys.py
@@ -1,8 +1,8 @@
 from comm.unit.unit_convert import *
 
 # SI: international unit
-kb_SI = 1.38064852e-23
-
+kb_SI = 1.38064852e-23 # Boltzmann constant
+c_SI = 2.99792458e+8 # speed of light in vacuum
 
 
 
@@ -12,4 +12,5 @@ kb_SI = 1.38064852e-23
 
 
 # Hau: Hartree atomic unit
-kb_Hau = kb_SI * Joule_to_hau / Kelvin_to_hau
+kb_Hau = kb_SI * Joule_to_Hau / Kelvin_to_Hau
+c_Hau = c_SI * meter_to_Hau/ second_to_Hau
